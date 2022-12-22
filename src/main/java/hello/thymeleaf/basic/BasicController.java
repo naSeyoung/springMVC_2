b0ae0458e2a4a28d10599d4fedf858e295c2401b
sktpdud97 <sktpdud97@naver.com>
2022-12-22 14:22:42 +0900

    20221222 주석

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -110,6 +110,11 @@ public class BasicController {
         addUsers(model);
         return "basic/condition";
     }
+    @GetMapping("/comments")
+    public String comments(Model model){
+        model.addAttribute("data","Spring!");
+        return "basic/comments";
+    }
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
